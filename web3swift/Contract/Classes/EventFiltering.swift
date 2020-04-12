ae74a86c09dbec703e2aaf27217d7fb0722948ed
Hwee-Boon Yar <hboon@motionobj.com>
2020-04-12 17:30:51 +0800

    Fix: event filtering to handle indexed and non-indexed fileds correctly

--- web3swift/Contract/Classes/EventFiltering.swift
+++ web3swift/Contract/Classes/EventFiltering.swift
@@ -75,19 +75,33 @@ internal func encodeTopicToGetLogs(contract: ContractV2, eventName: String?, fil
     }
     if filter.parameterFilters != nil {
         if event == nil {return nil}
-        var lastNonemptyFilter = 0
+        var lastNonemptyFilter = -1
         for i in 0 ..< filter.parameterFilters!.count {
             let filterValue = filter.parameterFilters![i]
             if filterValue != nil {
                 lastNonemptyFilter = i
             }
         }
-        if lastNonemptyFilter != 0 {
+        if lastNonemptyFilter != -1 {
             guard lastNonemptyFilter <= event!.inputs.count else {return nil}
             for i in 0 ... lastNonemptyFilter {
                 let filterValues = filter.parameterFilters![i]
-                let input = event!.inputs[i]
-                if filterValues != nil && !input.indexed {return nil}
+                if filterValues != nil {
+                    var isFound = false
+                    var targetIndexedPosition = i
+                    for j in 0 ..< event!.inputs.count{
+                        if event!.inputs[j].indexed {
+                            if targetIndexedPosition == 0 {
+                                isFound = true
+                                break
+                            }
+                            targetIndexedPosition -= 1
+                        }
+                    }
+
+                    if !isFound {return nil}
+                }
+
                 if filterValues == nil {
                     topics.append(nil as [String?]?)
                     continue
